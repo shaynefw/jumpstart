ee66f4ca83dcaca109143e19e00b116e43a64325
shaynefw <shaynefw@gmail.com>
2018-03-28 12:34:25 -0400

    add js slick

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
+});
