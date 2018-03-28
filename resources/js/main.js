cb1f0dd8f500920536a9d3b651ec17aec3d8839f
shaynefw <shaynefw@gmail.com>
2018-03-28 17:45:40 -0400

    complete navbar animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,4 +9,19 @@ $(document).ready(function(){
     pauseOnHover: false,
     pauseOnFocus: false,
   });
+
+// Navigation bar scroll animation
+
+  var mouseScroll = 0;
+  $(window).scroll(function(){
+    var scrollToTop = $(this).scrollTop();
+    if (scrollToTop - mouseScroll > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      mouseScroll = scrollToTop;
+    } else if (mouseScroll - scrollToTop > 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      mouseScroll = scrollToTop;
+    }
+  });
 });
